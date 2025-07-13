3a79e4d1555c1a3f7f42322475b5409c1bfa67b0
Bryan Alvarez <bryan.alvarez1251@gmail.com>
2025-07-13 08:17:17 -0700

    fixed compatibility issues

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { Camoufox, NewBrowser } from './sync_api.js';
 export { launchOptions } from './utils.js';
 export { launchServer } from './server.js';
+export { generateFingerprint, fromBrowserforge } from './fingerprints.js';
